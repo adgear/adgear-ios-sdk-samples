6c646f9c7b2cee445bba116b70b73cdaddbec45c
Boulat Oulmachev <oulmachev@gmail.com>
2017-11-10 14:06:42 -0500

    AdGearConsoleSample project initial commit

---
+++ AdGearConsoleSample/AdGearConsoleSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdGearConsoleSample
+//
+//  Created by Boulat Oulmachev on 2017-11-10.
+//  Copyright © 2017 AdGear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AdGearConsoleSample/AdGearConsoleSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AdGearConsoleSample
+//
+//  Created by Boulat Oulmachev on 2017-11-10.
+//  Copyright © 2017 AdGear. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
